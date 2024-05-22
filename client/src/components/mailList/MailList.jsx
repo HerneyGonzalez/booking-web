3743719ff4a4a75242e8dbd9004250f50582cecd
Santiago <u20191178305@usco.edu.co>
2024-05-22 13:20:40 -0500

    Envio de correo de suscripcion de ofertas

--- client/src/components/mailList/MailList.jsx
+++ client/src/components/mailList/MailList.jsx
@@ -1,16 +1,42 @@
-import "./mailList.css"
+import React, { useRef, useState } from 'react';
+import emailjs from '@emailjs/browser';
+import "./mailList.css";
 
 const MailList = () => {
-  return (
-    <div className="mail">
-      <h1 className="mailTitle">¡Ahorre tiempo, ahorre dinero!</h1>
-      <span className="mailDesc">Regístrate y te enviaremos las mejores ofertas</span>
-      <div className="mailInputContainer">
-        <input type="text" placeholder="example@example.com" />
-        <button>Suscribete</button>
-      </div>
-    </div>
-  )
-}
+  const form = useRef();
+  const [message, setMessage] = useState('');
 
-export default MailList
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('service_9yj0wgk', 'template_cpk1ke8', form.current, 'M_BlNb7tua4jwiQHw')
+      .then(
+        () => {
+          setMessage('Usuario suscrito');  // Mostrar mensaje de éxito
+          form.current.reset();  // Limpiar el formulario
+        },
+        (error) => {
+          console.error('Error al enviar el correo:', error.text);
+        }
+      );
+  };
+
+  return (
+    <div>
+      <form ref={form} onSubmit={sendEmail}>
+        <div className="mail">
+          <h1 className="mailTitle">¡Ahorre tiempo, ahorre dinero!</h1>
+          <span className="mailDesc">Regístrate y te enviaremos las mejores ofertas</span>
+          <label>Correo Electronico</label>
+          <input type="email" name="user_email" placeholder="example@example.com" required />
+          <input type="submit" value="Suscribete" />
+          {message && <p>{message}</p>}
+        </div>
+      </form>
+      
+    </div>
+  );
+};
+
+export default MailList;
