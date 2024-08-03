69234572dff15b811bc4bee9a0956c1d58ead7c9
Herney Gonzalez <90647465+HerneyGonzalez@users.noreply.github.com>
2024-08-03 16:07:32 -0500

    condicionar para ver si la data no existe

--- client/src/components/featuredProperties/FeaturedProperties.jsx
+++ client/src/components/featuredProperties/FeaturedProperties.jsx
@@ -2,7 +2,7 @@ import useFetch from "../../hooks/useFetch";
 import "./featuredProperties.css";
 
 const FeaturedProperties = () => {
-  const { data, loading} = useFetch("/hotels?featured=true&limit=4");
+  const { data, loading} = useFetch("hotels?featured=true&limit=4");
 
   return (
     <div className="fp">
@@ -10,7 +10,7 @@ const FeaturedProperties = () => {
         "Cargando"
       ) : (
         <>
-          {data.map((item) => (
+          {data && data.map((item) => (
             <div className="fpItem" key={item._id}>
               <img
                 src={item.photos[0]}
